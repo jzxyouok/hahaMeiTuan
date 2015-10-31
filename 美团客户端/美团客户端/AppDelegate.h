0503d380f8e00201eab40e705b5c3c7eb156aa37
gaojun <2293209493@qq.com>
2015-10-31 12:28:31 +0800

    新建工程

---
+++ 美团客户端/美团客户端/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  美团客户端
+//
+//  Created by ma c on 15/10/31.
+//  Copyright (c) 2015年 bjsxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
